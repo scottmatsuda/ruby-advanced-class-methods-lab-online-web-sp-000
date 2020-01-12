7cbafec6ed3d9293a3a61d4e2af0d4c0fafea5e6
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-12 19:03:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,4 +53,13 @@ class Song
     song.name = song_data[1]
     song
   end
+  
+  def self.create_from_filename(filename)
+    slice = filename.slice(0..filename.length - 5)
+    song_data = slice.split(" - ")
+    song = Song.new
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+    song.save
+  end
 end
