0f2da41a7d0df82445c778ad00b287b298931335
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-12 18:50:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    filename.slice(0..filename.length - 4)
+    filename.slice(0..filename.length)
     p filename
     song_array = filename.split(" - ").join(" ")
     p song_array
