36a44285038181b32bdf9e78d3367907821d186e
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-12 18:04:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,20 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize 
+    @@all << self
+  end
+
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    self
   end
 
 end
